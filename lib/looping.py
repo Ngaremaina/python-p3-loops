e626dfb3b30903a4f089112cfce19182dcb7f630
Ngaremaina <ngaremaina4@gmail.com>
2023-05-16 08:29:59 +0300

    Completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+    
 def square_integers(int_list):
-    # code goes here!
-    pass
+   numbers  =  [num ** 2 for num in int_list]
+   return numbers
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        print(fizzBuzz(i))  
+        i=i+1
+
+def fizzBuzz(i):
+    if i % 3 == 0 and i % 5 == 0:
+        return "FizzBuzz"
+    elif i % 3 == 0:
+        return "Fizz"
+    elif i % 5 == 0:
+        return "Buzz"
+    else:
+        return i
